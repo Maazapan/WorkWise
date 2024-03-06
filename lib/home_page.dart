5776c500f10145b1b08241ef92e2fe762b3ee2ad
Maazapan <barrioshtml@hotmail.com>
2024-03-06 00:05:34 -0700

    Home page en progreso

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -12,22 +16,64 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          //  backgroundColor: Colors.blue[300],
+        toolbarHeight: 190,
+        backgroundColor: const Color.fromARGB(134, 33, 191, 253),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(60),
+            bottomRight: Radius.circular(60),
           ),
-      body: const Center(
-        child: Column(
+        ),
+        flexibleSpace: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 700.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Busqueda',
-                  prefixIcon: Icon(Icons.search),
+            const Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(top: 30),
+                child: Text(
+                  'W o r k W i s e',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontFamily: 'adelia',
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(left: 60, right: 60, bottom: 45),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(80),
+                ),
+                child: const Padding(
+                  // 10
+                  padding: EdgeInsets.only(left: 33, right: 33, bottom: 5),
+                  child: TextField(
+                    cursorColor: Color.fromARGB(134, 33, 191, 253),
+                    decoration: InputDecoration(
+                      hintText: 'Buscador...',
+                      border: InputBorder.none,
+                      prefix: Padding(padding: EdgeInsets.only(top: 10)),
+                      prefixIcon: Icon(
+                        Icons.search,
+                        size: 25,
+                        color: Color.fromARGB(134, 33, 191, 253),
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
           ],
+        ),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
         ),
       ),
     );
