5536040ef7635d9b43518bcca050071260056707
Maazapan <barrioshtml@hotmail.com>
2024-03-06 21:54:02 -0700

    Barra de abajo agregada

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -15,8 +12,9 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //  backgroundColor: Colors.black,
       appBar: AppBar(
-        toolbarHeight: 190,
+        toolbarHeight: 150,
         backgroundColor: const Color.fromARGB(134, 33, 191, 253),
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.only(
@@ -27,42 +25,45 @@ class _MyHomePageState extends State<MyHomePage> {
         flexibleSpace: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Expanded(
-              child: Padding(
-                padding: EdgeInsets.only(top: 30),
-                child: Text(
-                  'W o r k W i s e',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.white,
-                    fontFamily: 'adelia',
-                  ),
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 90, right: 90, top: 50, bottom: 30),
+              child: Text(
+                'W o r k W i s e',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontFamily: 'adelia',
                 ),
               ),
             ),
             Expanded(
               child: Container(
-                margin: const EdgeInsets.only(left: 60, right: 60, bottom: 45),
+                margin: const EdgeInsets.only(left: 60, right: 60, bottom: 30),
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(80),
                 ),
-                child: const Padding(
-                  // 10
-                  padding: EdgeInsets.only(left: 33, right: 33, bottom: 5),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(left: 10, right: 33, bottom: 5),
                   child: TextField(
-                    cursorColor: Color.fromARGB(134, 33, 191, 253),
+                    cursorColor: const Color.fromARGB(134, 33, 191, 253),
                     decoration: InputDecoration(
-                      hintText: 'Buscador...',
+                      hintText: 'Trabajo Pesca, Diseño, Software, etc',
+                      hintStyle: TextStyle(
+                          color: Colors.black.withOpacity(0.1), fontSize: 17),
                       border: InputBorder.none,
-                      prefix: Padding(padding: EdgeInsets.only(top: 10)),
-                      prefixIcon: Icon(
-                        Icons.search,
-                        size: 25,
-                        color: Color.fromARGB(134, 33, 191, 253),
-                      ),
+                      prefixIcon: const Padding(
+                          padding: EdgeInsets.only(top: 3),
+                          child: Icon(
+                            Icons.search,
+                            size: 25,
+                            color: Color.fromARGB(134, 33, 191, 253),
+                          )),
                     ),
+                    style: const TextStyle(color: Colors.black45),
                   ),
                 ),
               ),
@@ -75,6 +76,46 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black.withOpacity(0.01),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 30,
+            ),
+            label: "Principal",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.work,
+              size: 30,
+              color: Color.fromRGBO(0, 0, 0, 0.2),
+            ),
+            label: "Trabajos Guardados",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              size: 30,
+              color: Color.fromRGBO(0, 0, 0, 0.2),
+            ),
+            label: "Cuenta",
+          ),
+        ],
+        selectedLabelStyle: const TextStyle(
+          fontSize: 13,
+        ),
+        unselectedLabelStyle: const TextStyle(
+          fontSize: 13,
+          color: Colors.blue,
+        ),
+        selectedItemColor: const Color.fromARGB(134, 33, 191, 253),
+        selectedFontSize: 15,
+        unselectedFontSize: 15,
+        elevation: 0.0,
       ),
     );
   }
